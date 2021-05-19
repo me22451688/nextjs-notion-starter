53dcf7d042cf8ec3e261ee44cf188cfa7a548d88
自用hosts <22451688@qq.com>
2021-05-19 18:50:19 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: favicon.ico,
+  defaultPageIcon: https://gitpages.gitee.io/hncaa.ico,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
